99b0a8be8bf44e57a8a487f6098482a9c653a05b
youssef <yousseflatr65@gmail.com>
2026-05-11 16:11:37 +0200

    setup everything add migrations freindships and freind_requests

---
+++ database/migrations/2026_05_02_122118_create_friend_requests_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('friend_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sender_id')->constrained("users")->onDelete("cascade");
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
+            $table->string("status")->default('pending');  // pending, accepted, declined
+            $table->timestamps();
+            $table->unique(['sender_id', 'receiver_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('friend_requests');
+    }
+};
